f5d55d6599222adf6a671919469461d42d81112f
Ryan Gupta <ryangpt@umich.edu>
2026-09-06 16:54:15 -0400

    Update: find-minimum-in-rotated-sorted-array - submission-1

--- Data Structures & Algorithms/find-minimum-in-rotated-sorted-array/submission-1.py
+++ Data Structures & Algorithms/find-minimum-in-rotated-sorted-array/submission-1.py
@@ -4,10 +4,13 @@ class Solution:
         right = len(nums) - 1
 
         while left < right:
-            mid = (left + right) // 2
+            mid = (left+right) // 2
+
+            #check to see if right side is sorted
             if nums[mid] > nums[right]:
                 left = mid + 1
-            else:
-                right = mid
-                
-        return nums[left]
+            else:              #otherwise its in the left side
+                right = mid    #min is at mid or in left
+
+        return nums[left]      #will become mid
+

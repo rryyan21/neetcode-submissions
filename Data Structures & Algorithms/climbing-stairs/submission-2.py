eb7e688e8ae7c6588320263b3c7c791abf65ea56
Ryan Gupta <ryangpt@umich.edu>
2026-09-13 16:30:08 -0400

    Add: climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/climbing-stairs/submission-2.py
@@ -0,0 +1,18 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        memo = {}
+        
+        def dp(n):
+            if n in memo:
+                return memo[n]
+            
+            if n == 1:
+                return 1
+
+            if n == 2:
+                return 2
+
+            memo[n] = dp(n-1) + dp(n-2)
+            return memo[n]
+        
+        return dp(n)
